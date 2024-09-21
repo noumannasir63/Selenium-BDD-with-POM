9abebe2467e0addb2befd52e8c8e0d2e1efdb346
Nouman Nasir <noumannasir63@gmail.com>
2024-09-21 11:45:03 +0500

    Modifying the Project Structure and Reusing the Code.

--- Steps/Common_Steps.py
+++ Steps/Common_Steps.py
@@ -15,7 +15,7 @@ def step_impl(context):
 def step_impl(context):
     context.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-
+"""
 @then(u'Verify that elements window is opened')
 def step_impl(context):
     element = context.driver.find_element(By.XPATH, "//div[@class='col-12 mt-4 col-md-6']")
@@ -25,3 +25,5 @@ def step_impl(context):
 
     # Assert that the actual text matches the expected text
     assert actual_text == expected_text, f"Expected text '{expected_text}', but got '{actual_text}'"
+    
+    """
